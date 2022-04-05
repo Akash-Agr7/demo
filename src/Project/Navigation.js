a940ff2c1b19c21169e55341dbf50aa94a34581b
admin <admin@admins-MacBook-Pro-5.local>
2022-04-05 15:51:32 +0530

    trial2

--- src/Project/Navigation.js
+++ src/Project/Navigation.js
@@ -1,75 +1,80 @@
 import {
-    FlatList,
-    Image,
-    Modal,
-    SafeAreaView,
-    StyleSheet,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    View,
-    Button,
-  } from 'react-native';
+  FlatList,
+  Image,
+  Modal,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Button,
+} from 'react-native';
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import {HomeScreen} from './HomeScreen'
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './HomeScreen';
+import Login from './Login';
 
 const Stack = createNativeStackNavigator();
 
 const MyStack = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator >
-                <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
 
         <Stack.Screen name="Details" component={DetailsScreen} />
-        {/* <Stack.Screen name="login" component={Login} /> */}
-
+        <Stack.Screen name="login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
+// function HomeScreen({ navigation }) {
+//     return (
+//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//         <Text>Home Screen</Text>
+//         <Button
+//           title="Go to Details"
+//           onPress={() => {
+//             /* 1. Navigate to the Details route with params */
+//             navigation.navigate('Details', {
+//               itemId: 86,
+//               otherParam: 'anything you want here',
+//             });
+//           }}
+//         />
+//       </View>
+//     );
+//   }
 
-function HomeScreen({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => {
-            /* 1. Navigate to the Details route with params */
-            navigation.navigate('Details', {
-              itemId: 86,
-              otherParam: 'anything you want here',
-            });
-          }}
-        />
-      </View>
-    );
-  }
-  
-  function DetailsScreen({ route, navigation }) {
-    /* 2. Get the param */
-    const { itemId, otherParam } = route.params;
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Text>itemId: {JSON.stringify(itemId)}</Text>
-        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-        <Button
-          title="Go to Details...... again"
-          onPress={() =>
-            navigation.push('Details', {
-              itemId: Math.floor(Math.random() * 100),
-            })
-          }
-        />
-        <Button title="Go back to Home" onPress={() => navigation.navigate('Home')} />
-        <Button title="Go back" onPress={() => navigation.goBack()} />
-      </View>
-    );
-  }
+function DetailsScreen({route, navigation}) {
+  /* 2. Get the param */
+  const {itemId, otherParam} = route.params;
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details..... again"
+        onPress={() =>
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+          })
+        }
+      />
+      <Button
+        title="Go back to login"
+        onPress={() => navigation.navigate('login')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
 
-  export default MyStack;
+export default MyStack;
